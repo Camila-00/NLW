6448f74f315e94ebb5e725a4ed9cad9c61513857
Camila-00 <53484905+Camila-00@users.noreply.github.com>
2020-06-05 18:27:49 -0300

    :hammer: Implementando o Nodemon

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 const express = require("express")
 const server = express()
 
-// Configurar caminhos da aplicação
+// #########   Configurar caminhos da aplicação    ###########
+
+
+//Configurar pasta public
+
+server.use(express.static("public"))
+
+//Configurar página Inicial
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+//Rota para create-point
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
 
 // Ligando o servidor
 
